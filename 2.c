1691cb92fb7467a8fba5936a4e6d2689fb2a8193
XDarren11 <jhosiescobar@gmail.com>
2023-04-19 20:55:00 -0600

    actualizado

--- 2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
-
-int main(void) {
-  printf("Holaa");
-
-}
---
+++ printPpid.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]) {
+  printf("El pid del proceso pradre es: %d\n", getpid());
+
+}
